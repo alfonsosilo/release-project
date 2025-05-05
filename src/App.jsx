d5d04597103d46a4ea73b0019bde6ab3363d0cf2
Alfonso Serrano <alfonso@silocreativo.com>
2025-05-05 10:16:40 +0200

    style(Inicio): Añadir nueva linea de lorem

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ function App() {
     <>
       <h1>Esta es mi pagina de inicio</h1>
       <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus eaque culpa fugit, eveniet ducimus maiores, saepe minima quod quidem iure necessitatibus atque adipisci alias rerum, sapiente perspiciatis ab earum. Veniam.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio labore tempora esse aspernatur eum. Assumenda distinctio inventore rerum reprehenderit voluptatum repellendus fugiat eius veritatis ad, doloribus ipsam sequi cupiditate omnis!</p>
     </>
   )
 }
