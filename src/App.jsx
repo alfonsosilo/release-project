65efe2a22431601e1f10dcaaca97d70ffd375ddd
Alfonso Serrano <alfonso@silocreativo.com>
2025-05-05 10:08:41 +0200

    chore(style): Añadir lorem ipsum a inicio

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
   return (
     <>
       <h1>Esta es mi pagina de inicio</h1>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus eaque culpa fugit, eveniet ducimus maiores, saepe minima quod quidem iure necessitatibus atque adipisci alias rerum, sapiente perspiciatis ab earum. Veniam.</p>
     </>
   )
 }
