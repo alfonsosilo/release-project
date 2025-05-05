1a18919be987fbfa209d033886ec265df62a8ee5
Alfonso Serrano <alfonso@silocreativo.com>
2025-05-05 14:25:10 +0200

    Cambiar nombre del boton

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
       <h1>Esta es mi pagina de inicio</h1>
-      <button className="btn btn-primary">Botón de ejemplo</button>
+      <button className="btn btn-primary">Nuevo</button>
       <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus eaque culpa fugit, eveniet ducimus maiores, saepe minima quod quidem iure necessitatibus atque adipisci alias rerum, sapiente perspiciatis ab earum. Veniam.</p>
       <p>Versión de la App: <VersionDisplay /></p>
     </>
