ee53e86b7cdacd7d16195eb9e34543f4d47e1d6e
Alfonso Serrano <alfonso@silocreativo.com>
2025-05-05 14:25:27 +0200

    hide: Eliminar clase del boton

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
       <h1>Esta es mi pagina de inicio</h1>
-      <button className="btn btn-primary">Nuevo</button>
+      <button>Nuevo</button>
       <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus eaque culpa fugit, eveniet ducimus maiores, saepe minima quod quidem iure necessitatibus atque adipisci alias rerum, sapiente perspiciatis ab earum. Veniam.</p>
       <p>Versión de la App: <VersionDisplay /></p>
     </>
